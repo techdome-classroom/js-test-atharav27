f091c6b3c6162f724efc13d278e59560453b58bf
atharav27 <110390023+atharav27@users.noreply.github.com>
2024-05-09 12:51:53 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,7 @@ const getTotalIsles = function (grid) {
   const cols = grid[0].length;
   let count = 0;
 
-  // Helper function for DFS e
+  // Helper fu
   function exploreIsland(row, col) {
       // Check if the current cell is within bounds and is land
       if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] === 'W') {
