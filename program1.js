4d763cf8bb06d27c5440c52559bb221ca47a7cba
atharav27 <110390023+atharav27@users.noreply.github.com>
2024-05-09 13:09:43 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -16,7 +16,6 @@ const getTotalIsles = function (grid) {
       
       grid[row][col] = 'W';
 
-
       exploreIsland(row + 1, col); 
       exploreIsland(row - 1, col)
       exploreIsland(row, col + 1); 
